c37e64a687678a816891f154465cc1a772498cbb
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-02-09 19:53:03 +0000

    setup: Initial changes for piston3.

--- setup.py
+++ setup.py
@@ -11,25 +11,33 @@ except ImportError:
 import os
 
 setup(
-    name = "django-piston",
+    name = "django-piston3",
     version = "0.2.3",
-    url = 'http://bitbucket.org/jespern/django-piston/wiki/Home',
-	download_url = 'http://bitbucket.org/jespern/django-piston/downloads/',
+  ##   url = 'http://bitbucket.org/jespern/django-piston/wiki/Home',
+	## download_url = 'http://bitbucket.org/jespern/django-piston/downloads/',
+    url = 'https://bitbucket.org/userzimmermann/django-piston3',
+    download_url = 'https://pypi.python.org/pypi/django-piston3',
     license = 'BSD',
-    description = "Piston is a Django mini-framework creating APIs.",
+    ## description = "Piston is a Django mini-framework creating APIs.",
+    description = "Piston for Python 3.3+ and Django 1.6+"
+                  " - Compatible with Python 2.7",
     author = 'Jesper Noehr',
     author_email = 'jesper@noehr.org',
-    packages = find_packages(),
-    namespace_packages = ['piston'],
+    maintainer = 'Stefan Zimmermann',
+    maintainer_email = 'zimmermann.code@gmail.com',
+    packages = [p.replace('piston', 'piston3') for p in find_packages()],
+    package_dir = {'piston3': 'piston'},
+    ## namespace_packages = ['piston'],
     include_package_data = True,
     zip_safe = False,
     classifiers = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
